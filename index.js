0ad2fb45469307e70cc6bd31b67d13d9167015bc
Doriano <doriano.bergamin18@gmail.com>
2024-01-13 11:39:54 +1100

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b){
+return a + b;
+}
+console.log(add(1, 2));
+
+function subtract(a, b){
+return a - b;
+}
+console.log(subtract(1, 2));
+
+function multiply(a, b){
+return a * b;
+}
+console.log(multiply(1, 2));
+
+function divide(a, b){
+return a / b;
+}
+console.log(divide(1, 2));
+
+function increment(a){
+    return a + 1;
+}
+console.log(increment(+1));
+
+
+function decrement(a){
+    return a - 1;
+}
+console.log(decrementt(-1));
+
+function makeInt(a){
+    return parseInt(a, 10);
+}
+console.log(makeInt("2"));
+
+function  preserveDecimal(a){
+    return parseFloat(a, 10.233455);
+}
+console.log(preserveDecimalt("2"));
